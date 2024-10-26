806ad9da6c5c7960f4f841405275c8e46ebeabdb
unknown <slavakaplin84@gmail.com>
2024-10-26 15:14:46 +1000

    add useDebounce hook

--- client/src/components/layout/Header/Header.tsx
+++ client/src/components/layout/Header/Header.tsx
@@ -1,17 +1,26 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import s from './header.module.scss'
 import { Container } from '@/components/shared/Container/Container'
 import { Search } from '@/icons/search'
 import { Layout } from '@/components/shared/Layout/Layout'
 import cn from 'classnames'
 import { useAppContext } from '@/hooks/useAppContext'
+import { useDebounce } from '@/hooks/useDebounce'
 export const Header = () => {
 	const [isFocusedInput, setIsFocusedInput] = useState<boolean>(false)
-	const { setSearchValue } = useAppContext()
+	const { searchValue, setSearchValue } = useAppContext()
+
+	const [inputValue, setInputValue] = useState(searchValue)
+	const debouncedValue = useDebounce(inputValue, 200)
+	useEffect(() => {
+		setSearchValue(debouncedValue)
+	}, [debouncedValue, setSearchValue])
+
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setSearchValue(event.target.value)
+		setInputValue(event.target.value)
 	}
+
 	return (
 		<header className={s.header}>
 			<Container>
